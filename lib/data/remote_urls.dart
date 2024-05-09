84df06f4b1ab18aec978d2c066981916e11ce3a3
Sadam Abdi <faarisapp@gmail.com>
2024-05-09 09:32:51 +0300

    update ios config

--- lib/data/remote_urls.dart
+++ lib/data/remote_urls.dart
@@ -1,6 +1,6 @@
 class RemoteUrls {
-  static const String rootUrl = 'http://160.119.251.252:3000';
-  //"http://159.223.81.156:3000";
+  static const String rootUrl = 'http://127.0.0.1:3000';
+  //"http://160.119.251.252:3000";
 
   static const login = '$rootUrl/auth/login';
   static const signup = '$rootUrl/users';
